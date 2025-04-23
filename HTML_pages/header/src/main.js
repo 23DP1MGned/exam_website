5317afce7a431d4dcdc5e4d2da3e095734930fa8
Maksims Gnedovs <166118392+23DP1MGned@users.noreply.github.com>
2025-04-23 19:49:36 +0000

    header mobile version beta

--- HTML_pages/header/src/main.js
+++ HTML_pages/header/src/main.js
@@ -22,3 +22,14 @@ document.addEventListener('mousemove', (e) => {
   mockupBar.style.transform = `translateY(${offsetY * maxTranslateVertical}px)`;
   mockupContent.style.transform = `translateY(${offsetY * maxTranslateVertical}px)`;
 });
+
+const menuButton = document.getElementById('menuButton');
+const menuNav = document.getElementById('menuNav');
+
+menuButton.addEventListener('click', function() {
+  if (menuNav.classList.contains('menu-hidden')) {
+    menuNav.classList.remove('menu-hidden');
+  } else {
+    menuNav.classList.add('menu-hidden');
+  }
+});
