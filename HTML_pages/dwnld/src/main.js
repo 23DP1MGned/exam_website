4fbc6ea83f675b608f2d9f4be80d3f62e1d9f2e2
Maksims Gnedovs <166118392+23DP1MGned@users.noreply.github.com>
2025-04-28 16:50:53 +0000

    dwnld page mobile version added

--- HTML_pages/dwnld/src/main.js
+++ HTML_pages/dwnld/src/main.js
@@ -8,3 +8,14 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 200);
   });
   
+
+const menuButton = document.getElementById('menuButton');
+const menuNav = document.getElementById('menuNav');
+
+menuButton.addEventListener('click', function() {
+  if (menuNav.classList.contains('menu-hidden')) {
+    menuNav.classList.remove('menu-hidden');
+  } else {
+    menuNav.classList.add('menu-hidden'); 
+  }
+});
